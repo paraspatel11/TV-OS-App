9913a0ea4abf48c5344ccc310131159906987de1
Paras <paras1103patel@gmail.com>
2019-11-26 15:15:54 -0500

    Initial Commit

---
+++ Assignment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by Xcode User on 2019-11-26.
+//  Copyright © 2019 Xcode User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
